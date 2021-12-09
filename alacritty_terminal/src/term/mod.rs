2dda25230ce93bd3f4ba1f6038bf612372128cc3
Joseph Benden <joe@benden.us>
2021-12-09 16:21:06 -0700

    fix(sixels): useless use of `format!`
    Signed-off-by: Joseph Benden <joe@benden.us>

--- alacritty_terminal/src/term/mod.rs
+++ alacritty_terminal/src/term/mod.rs
@@ -1816,8 +1816,7 @@ impl<T: EventListener> Handler for Term<T> {
             self.event_proxy.send_event(Event::PtyWrite(text));
         }
 
-        let trailer_text = format!("S");
-        self.event_proxy.send_event(Event::PtyWrite(trailer_text));
+        self.event_proxy.send_event(Event::PtyWrite("S".to_string()));
     }
 
     fn start_sixel_graphic(&mut self, params: &Params) -> Option<Box<sixel::Parser>> {
